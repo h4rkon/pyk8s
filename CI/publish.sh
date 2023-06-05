e680d1ef2c65e8ba408761fd3fde4e42ba0d4431
Victor Sauermann <victor.sauermann@gmail.com>
2023-06-05 20:32:05 +0200

    executable publish shell script

--- CI/publish.sh
+++ CI/publish.sh
@@ -1,1 +1,7 @@
-echo ""
+#!/bin/bash
+
+bumpversion patch
+docker build -t h4rkon/pyk8s:`cat VERSION` .
+docker tag h4rkon/pyk8s:`cat VERSION` h4rkon/pyk8s:latest
+docker push h4rkon/pyk8s:`cat VERSION`
+docker push h4rkon/pyk8s:latest
